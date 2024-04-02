40bbf3d273d51b6ab50d362f3184d7362fd02c3f
odlmarce <delmarcelle.olivier@gmail.com>
2024-04-02 22:32:47 +0200

    set version for install

--- gremlin-python/src/main/python/setup.py
+++ gremlin-python/src/main/python/setup.py
@@ -36,7 +36,7 @@ if not os.path.exists(os.path.join(root, 'PKG-INFO')):
     fd.write("'''")
     fd.write(__doc__)
     fd.write("'''\n")
-    fd.write('version   = %r\n' % os.getenv('VERSION', '?').replace('-SNAPSHOT', '.dev-%d' % timestamp))
+    fd.write('version   = %r\n' % os.getenv('VERSION', '3.7.1').replace('-SNAPSHOT', '.dev-%d' % timestamp))
     fd.write('timestamp = %d\n' % timestamp)
     fd.close()
 # Load version
